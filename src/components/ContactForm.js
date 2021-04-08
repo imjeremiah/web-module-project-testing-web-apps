770a37fc97add2892b6296e54db5941ccb634c58
Jeremiah Candelaria <jeremiahcandelaria@gmail.com>
2021-04-08 14:52:09 -0700

    create useForm hook to separate state

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import DisplayComponent from './DisplayComponent';
+
+import useForm from "../hooks/useForm";
 
 const formData = {
   firstName: "",
@@ -16,57 +18,8 @@ const errorData = {
 }
 
 const ContactForm = () => {
-  const [displayData, setDisplayData] = useState(false);
-  const [form, setForm] = useState(formData);
-  const [errors, setErrors] = useState(errorData);
-
-  const errorHandling = (fieldName, fieldValue) => {
-    if (fieldName === "firstName" && fieldValue.length < 5)
-      return `${fieldName} must have at least 5 characters.`;
-
-    const emailRegex = /(.*)@(.*)\.(.+)/g;
-    if (fieldName === "email" && !fieldValue.match(emailRegex))
-      return `${fieldName} must be a valid email address.`;
-
-    if (fieldName !== "message" && fieldValue === "")
-      return `${fieldName} is a required field.`;
-    
-    return "";
-  }
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const submitErrors = {};
-    Object.keys(errors).forEach(field => {
-      submitErrors[field] = errorHandling(field, form[field])
-    });
-    
-    setErrors(submitErrors);
-    
-    const hasErrors = (submitErrors.firstName === "" && submitErrors.lastName === "" && submitErrors.email === "" && submitErrors.message === "");
-    setDisplayData(hasErrors);
-      
-  };
-
-  const handleChange = (e) => {
-    const errorMessage = errorHandling(e.target.name, e.target.value);
-
-    if (errorMessage !== "") {
-      setDisplayData(false);
-    }
-
-    setErrors({
-      ...errors,
-      [e.target.name]: errorMessage
-    });
-
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value
-    });
-  }
+  
+  const [ displayData, form, errors, handleSubmit, handleChange ] = useForm(formData, errorData);
 
   return (
     <div className="App">
